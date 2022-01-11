3f0d53386c0287532843ccba614b5d13212210eb
Abigail Cotter <abigail.cotter@gmail.com>
2022-01-10 21:18:10 -0500

    add checker lambda to see if videos been processed

---
+++ endpointLambdas/isProcessVideoChecker/IsProcessedVideo.py
@@ -0,0 +1,37 @@
+import pymysql
+import json
+import RDSconfigs
+
+rdsHost = RDSconfigs.db_endpoint
+name = RDSconfigs.db_username
+password = RDSconfigs.db_password
+dbName = RDSconfigs.db_name
+port = 3306
+
+
+conn = pymysql.connect(host=rdsHost,user=name,
+												passwd=password,db=dbName,
+												connect_timeout=5,
+												cursorclass=pymysql.cursors.DictCursor)
+
+
+# inputs
+# {
+# 	youtubeVideoId: string,
+# }
+def lambda_handler(event, context):
+	videoId = event['youtubeVideoId']
+	with conn.cursor() as cur:
+			qry = f'SELECT * FROM Videos WHERE video_id = {videoId}'
+			cur.execute(qry)
+			row = cur.fetchall()
+	return {
+			'statusCode': 200,
+			'headers': {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+					'Access-Control-Allow-Credentials': 'true',
+					'Content-Type': 'application/json'
+			},
+			'body': json.dumps(row)
+	}
