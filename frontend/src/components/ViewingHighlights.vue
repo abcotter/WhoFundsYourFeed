d8d002796de1bb5770647972c9a3057c98fdcd2b
nb03 <n6butt@uwaterloo.ca>
2022-01-23 20:15:01 -0500

    hover works, positioning of elements is messed up

--- frontend/src/components/ViewingHighlights.vue
+++ frontend/src/components/ViewingHighlights.vue
@@ -5,21 +5,32 @@
 			<!--<h2><font color="#292f36">My Viewing Stats</font></h2>-->
 			
 			<div class="statCard1">
-			<div class="statCard1Front">
+			<div class="statCard1Content statCard1Front">
 				<p> Stat 1 Front</p>
 			</div>
-			<div class="statCard1Back">
-				<p> Stat 1 Back </p>
+			<div class="card-body">
+				<p> t </p>
+				</div>
+			<div class="statCard1Content statCard1Back">
+				<p> Stat 1 Back</p>
+				</div>
+				<div class="card-body">
+					<p> t </p>
 				</div>
 			</div>
-			
 
 			<div class="influencerCard1">
-			<div class="influencerCard1Front">
+			<div class="influencerCard1Content influencerCard1Front">
 				<p> Influencer 1 Front</p>
 			</div>
-			<div class="influencerCard1Back">
+			<div class="card-body">
+				<p> h </p>
+			</div>
+			<div class="influencerCard1Content influencerCard1Back">
 				<p> Influencer 1 Back</p>
+				</div>
+				<div class="card-body">
+					<p>t</p>
 				</div>
 			</div>
 
@@ -52,7 +63,10 @@
 <script>
 export default {
   name: 'Main',
+   
 };
+
+ 
 </script>
 
 <style scoped>
@@ -75,8 +89,19 @@ export default {
 
 .statCard1{
 	display: flex;
+	perspective: 1000px;
+	position: relative;
 }
-
+.statCard1Content{ 
+	box-shadow: 0 1rem 1Srem rgba(0,0,0,.5);
+	transition: all 1s;
+	position: absolute;
+	top:0;
+	left:0;
+	width: 100%;
+	backface-visibility: hidden;
+	-webkit-backface-visibility: hidden;
+}
 .statCard1Front{
 	flex:1;
 	background-color: rgb(247,255,247);
@@ -92,6 +117,13 @@ export default {
 	padding: 10px;
 	margin: 5px;
 	color: rgb(41, 47, 54);
+	transform:rotateY(180deg);
+}
+.statCard1:hover .statCard1Front{ 
+	transform: rotateY(-180deg);
+}
+.statCard1:hover .statCard1Back{
+	transform: rotateY(0);
 }
 
 .statCard2{
@@ -120,20 +152,44 @@ export default {
 	margin: 5px;
 	color: rgb(41, 47, 54)
 }
-
-
 .influencerCard1{
 	display: flex;
+	perspective: 1000px;
+	position: relative;
 }
-
+.influencerCard1Content{ 
+	box-shadow: 0 1rem 1Srem rgba(0,0,0,.5);
+	transition: all 1s;
+	position: absolute;
+	top:0;
+	left:0;
+	width: 100%;
+	backface-visibility: hidden;
+	-webkit-backface-visibility: hidden;
+}
 .influencerCard1Front{
 	flex:1;
-	background-color:rgb(255, 107, 107);
+	background-color: rgb(255, 107, 107);
 	border-radius: 25px;
 	padding: 10px;
 	margin: 5px;
-	color: rgb(41, 47, 54)
-
+	color: rgb(41, 47, 54);
+}
+.influencerCard1Back{
+	flex:1;
+	background-color: rgb(255, 107, 107);
+	border-radius: 25px;
+	padding: 10px;
+	margin: 5px;
+	color: rgb(41, 47, 54);
+	transform:rotateY(180deg);
+	
+}
+.influencerCard1:hover .influencerCard1Front{ 
+	transform: rotateY(-180deg);
+}
+.influencerCard1:hover .influencerCard1Back{
+	transform: rotateY(0);
 }
 
 
