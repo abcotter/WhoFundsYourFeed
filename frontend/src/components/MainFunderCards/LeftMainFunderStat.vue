54aec58075d762088c797b902bdc0955889311d8
Abigail Cotter <abigail.cotter@gmail.com>
2022-01-24 22:03:31 -0500

    add flipping of content

--- frontend/src/components/MainFunderCards/LeftMainFunderStat.vue
+++ frontend/src/components/MainFunderCards/LeftMainFunderStat.vue
@@ -1,8 +1,16 @@
 <template>
 	<div class="funderCard">
-		Left Stat <br />
-		maybe I contain a % statistic that says what amount of the videos you watch
-		does this funder sponser
+		<div class="content-flipper" @click="flip">
+			<div class="front" :class="{ reveal: flipped }">
+				<div>Left Stat</div>
+			</div>
+			<div class="back" :class="{ reveal: flipped }">
+				<div>
+					maybe I contain a % statistic that says what amount of the videos you
+					watch does this funder sponser
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -10,15 +18,61 @@
 //define components here that can be used elsewhere
 export default {
 	name: "LeftMainFunderStat",
+	data() {
+		return {
+			flipped: false,
+		};
+	},
+	methods: {
+		flip() {
+			this.flipped = !this.flipped;
+		},
+	},
 };
 </script>
 
-<style scoped>
+<style>
 .funderCard {
-	background-color: rgb(247, 255, 247);
-	border-radius: 25px;
-	padding: 10px;
 	margin: 10px;
 	width: 40%;
+}
+
+.content-flipper {
+	border-radius: 25px;
+	padding: 10px;
+	background-color: rgb(247, 255, 247);
+	width: 100%;
+	height: 100%;
+	transition: ease-in 300ms;
+	transform-style: preserve-3d;
+}
+
+.flip {
+	transform: rotateY(180deg);
+}
+
+.front {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
+}
+
+.front.reveal {
+	transform: rotateY(180deg);
+}
+
+.back {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
+	transform: rotateY(180deg);
+}
+
+.back.reveal {
+	transform: rotateY(0deg);
 }
 </style>
