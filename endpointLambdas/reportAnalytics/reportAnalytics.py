3c430a840b29f53703c200351bfd55275ed9a44e
FatimahAreola <fatimahyinka@gmail.com>
2022-01-28 20:41:02 -0500

    Lambda uploaded to AWS

--- endpointLambdas/reportAnalytics/reportAnalytics.py
+++ endpointLambdas/reportAnalytics/reportAnalytics.py
@@ -20,10 +20,9 @@ conn = pymysql.connect(host=rdsHost, user=name,
 
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    userId = body['userId']
-
-    reportOutputJSON = []
+    userId = json.loads(event['userId'])
+    userId = str(userId)
+    reportOutputJSON = {}
 
     # % of time watched that is sponsored vs not
     """SELECT SUM(CASE WHEN is_sponsored = TRUE THEN video_duration_secs ELSE 0 END) *100 /SUM(video_duration_secs)
@@ -33,8 +32,7 @@ def lambda_handler(event, context):
                 ORDER BY time_watched DESC LIMIT 50) as watchHistory;"""
 
     with conn.cursor() as cur:
-        qryTimeSponsored = f" SELECT SUM(CASE WHEN is_sponsored = TRUE THEN video_duration_secs ELSE 0 END) *100 /SUM(video_duration_secs) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id = " + \
-            userId + "ORDER BY time_watched DESC LIMIT 50) as watchHistory;"
+        qryTimeSponsored = f"SELECT SUM(CASE WHEN is_sponsored = TRUE THEN video_duration_secs ELSE 0 END) *100 /SUM(video_duration_secs) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id =" + userId + " ORDER BY time_watched DESC LIMIT 50) as watchHistory;"
 
         try:
             cur.execute(qryTimeSponsored)
@@ -43,11 +41,12 @@ def lambda_handler(event, context):
 
         except pymysql.Error as e:
             outputTimeSponsored = "System Error"
+            print(e)
             return {
-                'statusCode': 500,
+                'statusCode': 500,    
             }
     
-        reportOutputJSON[outputTimeSponsored] = outputTimeSponsored        
+        reportOutputJSON['outputTimeSponsored'] = outputTimeSponsored        
 
         # % of videos watched that are sponsored
         """ SELECT SUM(CASE WHEN is_sponsored THEN 1 ELSE 0 END)*100 / COUNT(*)
@@ -59,8 +58,7 @@ def lambda_handler(event, context):
                                 LIMIT 50) as userWatched """
 
     with conn.cursor() as cur:
-        qryVideoSponsored = f" SELECT SUM(CASE WHEN is_sponsored THEN 1 ELSE 0 END)*100 / COUNT(*) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id = " + \
-            userId + "ORDER BY time_watched DESC LIMIT 50) as userWatched"
+        qryVideoSponsored = f" SELECT SUM(CASE WHEN is_sponsored THEN 1 ELSE 0 END)*100 / COUNT(*) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id = " + userId + " ORDER BY time_watched DESC LIMIT 50) as userWatched"
 
         try:
             cur.execute(qryVideoSponsored)
@@ -68,11 +66,12 @@ def lambda_handler(event, context):
 
         except pymysql.Error as e:
             outputVideoSponsored = "System Error"
+            print(e)
             return {
                 'statusCode': 500,
             }
 
-        reportOutputJSON[outputVideoSponsored] = outputVideoSponsored
+        reportOutputJSON['outputVideoSponsored'] = outputVideoSponsored
 
         # Most frequently occuring companies
         """ SELECT brand_name 
@@ -89,9 +88,7 @@ def lambda_handler(event, context):
         LIMIT 5  """
 
     with conn.cursor() as cur:
-        qryFrequentCompanies = f" SELECT brand_name FROM who_funds_your_feed.Brands NATURAL JOIN who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id = " + \
-            userId + \
-            "ORDER BY time_watched DESC LIMIT 50) as userWatched NATURAL JOIN who_funds_your_feed.Sponsorships GROUP BY brand_name ORDER BY COUNT(brand_name) DESC LIMIT 5 "
+        qryFrequentCompanies = f" SELECT brand_name FROM who_funds_your_feed.Brands NATURAL JOIN who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id =" + userId + " ORDER BY time_watched DESC LIMIT 50) as userWatched NATURAL JOIN who_funds_your_feed.Sponsorships GROUP BY brand_name ORDER BY COUNT(brand_name) DESC LIMIT 5 "
 
         try:
             cur.execute(qryFrequentCompanies)
@@ -99,11 +96,12 @@ def lambda_handler(event, context):
 
         except pymysql.Error as e:
             outputFrequentCompanies = "System Error"
+            print(e)
             return {
                 'statusCode': 500,
             }
  
-        reportOutputJSON[outputFrequentCompanies] = outputFrequentCompanies
+        reportOutputJSON['outputFrequentCompanies'] = outputFrequentCompanies
 
         # our top channels top sponsors (channels you watch a lot and who are their sponsors)
         """ SELECT brand_name
@@ -123,7 +121,7 @@ def lambda_handler(event, context):
         LIMIT 5"""
 
     with conn.cursor() as cur:
-        qryChannelSponsors = f"SELECT brand_name From who_funds_your_feed.Brands NATURAL JOIN who_funds_your_feed.Sponsorships NATURAL JOIN (SELECT video_id, channel_name FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id = " + userId + "ORDER BY time_watched DESC LIMIT 50) as userWatched WHERE is_sponsored = TRUE GROUP BY channel_name ORDER BY COUNT(channel_name) DESC LIMIT 5) AS topChannels WHERE Sponsorships.video_id = topChannels.video_id LIMIT 5"
+        qryChannelSponsors = f"SELECT brand_name From who_funds_your_feed.Brands NATURAL JOIN who_funds_your_feed.Sponsorships NATURAL JOIN (SELECT video_id, channel_name FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT * FROM who_funds_your_feed.Watches WHERE user_id =" + userId + " ORDER BY time_watched DESC LIMIT 50) as userWatched WHERE is_sponsored = TRUE GROUP BY channel_name ORDER BY COUNT(channel_name) DESC LIMIT 5) AS topChannels WHERE Sponsorships.video_id = topChannels.video_id LIMIT 5"
 
         try:
             cur.execute(qryChannelSponsors)
@@ -131,11 +129,12 @@ def lambda_handler(event, context):
 
         except pymysql.Error as e:
             outputChannelSponsors = "System Error"
+            print(e)
             return {
                 'statusCode': 500,
             }
 
-        reportOutputJSON[outputChannelSponsors] = outputChannelSponsors
+        reportOutputJSON['outputChannelSponsors'] = outputChannelSponsors
 
         # What makes you unique (your top categories)
         """SELECT video_category, count(video_category)
@@ -146,7 +145,7 @@ def lambda_handler(event, context):
                 GROUP BY video_category
                 ORDER BY COUNT(video_category) DESC """
     with conn.cursor() as cur:
-        qryTopCategories = f" SELECT video_category, count(video_category) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT video_id FROM who_funds_your_feed.Watches WHERE user_id = " + userId + "ORDER BY time_watched DESC LIMIT 50) as userWatches GROUP BY video_category ORDER BY COUNT(video_category)"
+        qryTopCategories = f" SELECT video_category, count(video_category) FROM who_funds_your_feed.Videos NATURAL JOIN (SELECT video_id FROM who_funds_your_feed.Watches WHERE user_id =" + userId + " ORDER BY time_watched DESC LIMIT 50) as userWatches GROUP BY video_category ORDER BY COUNT(video_category)"
 
         try:
             cur.execute(qryTopCategories)
@@ -154,10 +153,11 @@ def lambda_handler(event, context):
 
         except pymysql.Error as e:
             outputTopCategories = "System Error"
+            print(e)
             return {
                 'statusCode': 500,
             }
 
-        reportOutputJSON[outputTopCategories] = outputTopCategories
+        reportOutputJSON['outputTopCategories'] = outputTopCategories
 
     body = json.dumps(reportOutputJSON)
