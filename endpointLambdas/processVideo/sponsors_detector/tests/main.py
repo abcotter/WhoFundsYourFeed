f2109af2974fa0597228bd0b61c058b06467b34f
CarlaLeal <lealramoscarla@gmail.com>
2022-01-31 15:07:04 -0500

    data

---
+++ endpointLambdas/processVideo/sponsors_detector/tests/main.py
@@ -0,0 +1,11 @@
+import pandas
+"""
+detect sponsors and save to list
+for every video, count how many of the sponsors are actually sponsors (precision)
+count what percentage of all tthe sponsors were actually found (recall)
+erturn and add to total precision
+
+
+"""
+if __name__ == "__main__":
+    return
