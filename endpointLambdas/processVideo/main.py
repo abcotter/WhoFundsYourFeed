6f806b1cb9a272dacf17cd0436565d17e6912f4a
CarlaLeal <lealramoscarla@gmail.com>
2022-02-02 15:46:42 -0500

    update results

--- endpointLambdas/processVideo/main.py
+++ endpointLambdas/processVideo/main.py
@@ -1,14 +1,11 @@
 import pandas as pd
+import json
 from sponsors_detector.process_text import find_video_sponsors
 import spacy
-"""
-detect sponsors and save to list
-for every video, count how many of the sponsors are actually sponsors (precision)
-count what percentage of all tthe sponsors were actually found (recall)
-erturn and add to total precision
-"""
+
 def get_actual_sponsors(video_id, data):
-    return data[data['video_id']==video_id]['brand_name'].tolist()
+    brand_names = data[data['video_id']==video_id]['brand_name'].tolist()
+    return [brand_name.lower() for brand_name in brand_names]
 
 if __name__ == "__main__":
     nlp = spacy.load("en_core_web_sm")
@@ -16,20 +13,35 @@ if __name__ == "__main__":
     video_ids = data['video_id'].unique().tolist()
     precision_metrics = []
     recall_metrics = []
+    overall_results = []
     for i, video_id in enumerate(video_ids):
-        if i == 1:
-            break
-        result = find_video_sponsors(video_id, nlp)
+        try:
+            result = find_video_sponsors(video_id, nlp)
+        except Exception as e:
+            print('Error finding sponsors', e)
+            continue
         sponsorships = result.get('sponsorships', [])
-        sponsorships = [s['name'] for s in sponsorships]
-        actual_sponsorships = get_actual_sponsors(video_id, data)
-        total = set(actual_sponsorships).intersection(set(sponsorships))
-        precision = len(total)/len(sponsorships)
-        recall = len(total)/len(actual_sponsorships)
-        print({'video_id': video_id,
-               'predictted': sponsorships,
-               'actual': actual_sponsorships,
-               'precision': precision,
-               'recall': recall})
+        sponsorships = [s['name'].lower() for s in sponsorships]
+        if len(sponsorships) == 0:
+            precision = 0.0
+            recall = 0.0
+        else:
+            actual_sponsorships = get_actual_sponsors(video_id, data)
+            total = set(actual_sponsorships).intersection(set(sponsorships))
+            precision = len(total)/len(sponsorships)
+            recall = len(total)/len(actual_sponsorships)
+        results_info = {'video_id': video_id,
+                        'predicted': sponsorships,
+                        'actual': actual_sponsorships,
+                        'precision': precision,
+                        'recall': recall}
+        overall_results.append(results_info)
+        precision_metrics.append(precision)
+        recall_metrics.append(recall)
+        print(json.dumps(results_info, indent=4))
+    with open('results.json', 'w') as f:
+        json.dump(overall_results, f)
+    print('average precision:', sum(precision_metrics)/len(precision_metrics))
+    print('average recall:', sum(recall_metrics)/len(recall_metrics))
 
 
--- endpointLambdas/processVideo/sponsors_detector/process_text.py
+++ endpointLambdas/processVideo/sponsors_detector/process_text.py
@@ -4,9 +4,9 @@ import re
 import requests
 
 YOUTUBE_API_BASE_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet"
-SOCIAL_MEDIA_DOMAINS = set(['instagram', 'facebook', 'linkedin', 'youtube',
-                              'snapchat', 'twitter', 'paypal', 'patreon', 'tiktok',
-                              'podcasts.apple', 'flickr', 'soundcloud', 'spotify'])
+SOCIAL_MEDIA_DOMAINS = set(['instagram', 'facebook', 'linkedin', 'youtube', 'snapchat', 'twitter', 'paypal', 'patreon', 'tiktok',
+                            'podcasts.apple', 'flickr', 'soundcloud', 'spotify',
+                            'twitch', 'twitter', 'reddit', 'thematic', 'amazon', 'discord'])
 
 YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 
@@ -23,6 +23,8 @@ def find_video_sponsors(video_id: str, model) -> list:
     if not urls:
         return result
     sponsorships = scrape_sponsor_websites(urls, model)
+    #sanity check
+    sponsorships = [sponsor for sponsor in sponsorships if sponsor["name"].lower() not in SOCIAL_MEDIA_DOMAINS]
     result["sponsorships"] = sponsorships
     result["youtubeApiResponse"] = response.json()
     return result
@@ -38,25 +40,34 @@ def get_video_description(youtube_api_response):
 def find_urls(description: str) -> set:
     """Find urls specifide in video description"""
     urls = set()
-    matches = re.findall('(https?://.*\.ly[^ ]*)', description)
-    for match in matches:
-        urls.add(match)
-
-    matches = re.findall(r'(https?://.*?([a-zA-Z]*).(com|ca)[^ ]*)', description)
+    # matches = re.findall('(https?://.*?\.ly[^ ]*)', description)
+    # for match in matches:
+    #     urls.add(match)
+    matches = re.findall(r'(https?://.*?([a-zA-Z]*).(com|ca|ly|us)[^ ]*)', description)
     for match in matches:
         if match[1] not in SOCIAL_MEDIA_DOMAINS:
             urls.add(match[0])
     return urls
+def get_html_content(url):
+    exception = False
+    try:
+        #could try https version
+        page = requests.get(url, timeout=30)
+    except Exception as e:
+        exception = True
+    if exception or not page.content or page.status_code == 404:
+        matches = re.search('(https?://.*?([a-zA-Z]*).(com|ca))', page.url)
+        page_url = matches.group(0) if matches else page.url
+        page = requests.get(page_url, timeout=30)
+    return page
 
 def scrape_sponsor_websites(urls: list, model):
     """Scrape urls and extract business name from website title"""
     sponsors = set()
     for url in urls:
         try:
-            page = requests.get(url)
+            page = get_html_content(url)
         except Exception as e:
-            continue
-        if not page.content:
             continue
         parser = BeautifulSoup(page.content, 'html.parser')
         title = parser.title
@@ -66,12 +77,12 @@ def scrape_sponsor_websites(urls: list, model):
         if not title:
             continue
         document = model(title.get_text())
-        entities = set()
+        entities = []
         for token in document:
             if token.text.lower() in domain and token.pos_ not in set(['PUNCT', 'ADP', 'DET']):
-                entities.add(token.text)
+                entities.append(token.text)
         if entities:
-            sponsors.add((' '.join(entities), url))
-    return [{"name": name, "url": page_url} for name, url in sponsors]
+            sponsors.add((' '.join(entities), page_url))
+    return [{"name": name, "url": url} for name, url in sponsors]
 
 
