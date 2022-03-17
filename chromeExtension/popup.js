16bebdd1d6d15e36c4bec33896f8f30d2c348fdc
nb03 <n6butt@uwaterloo.ca>
2022-03-17 19:04:14 -0400

    added UI changes requested by Carla

---
+++ chromeExtension/popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+	var y = document.getElementById("index_link");
+	y.addEventListener("click", openIndex);
+});
+
+function openIndex() {
+	userId = localStorage.getItem("userId");
+	baseUrl = "http://wfyf-app.s3-website-us-east-1.amazonaws.com/";
+	url = userId ? baseUrl + userId : baseUrl;
+	chrome.tabs.create({ active: true, url: url });
+}
